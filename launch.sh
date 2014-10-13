29ad0c4d06ad1f78166af77c370f9ea025b48778
steve <maslenni@gmail.com>
2014-10-13 04:27:20 +0400

    added the dictionary correction and AMorph14 training stages

--- launch.sh
+++ launch.sh
@@ -1,14 +1,21 @@
-#sudo cp ~/install/TurboParser-2.1.0/deps/local/lib/libgflags.so.2 /usr/lib/libgflags.so.2
-#sudo cp ~/install/TurboParser-2.1.0/deps/local/lib/libglog.so.0 /usr/lib/libglog.so.0
 python generateSentence.py "$1"
 make
-SESSION_MANAGER=''
-export DISPLAY=:0.0
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/home/steve/install/TurboParser-2.2.0/deps/local/lib:"
+
+# input the infinitive forms before verbs and gerunds in the dictionary file from opencorpora.org
+python correctDictionary.py ../openCorpora/dict.opcorpora.txt ../openCorpora/good_dict.opcorpora.txt
+
+# train the morphological parser AMorph14
+./genhashtable openCorpora/good_dict.opcorpora.txt hash.bin
+
+# train the syntactic parser ASynt14
 ./main --synttrain test1142.xml newmmodel.txt newsyntmodel.txt ~/install/TurboParser-2.1.0/TurboParser
-#./main --syntmark test1142.xml newmmodel.txt newsyntmodel.txt output.txt /home/steve/install/TurboParser-2.2.0/TurboParser
+
+# apply AMorph14 the morphological parser
 ./main --morphmark testSentence.xml morphmark_output.txt newmmodel.txt
+
+
+#./main --syntmark test1142.xml newmmodel.txt newsyntmodel.txt output.txt /home/steve/install/TurboParser-2.2.0/TurboParser
 ./main --syntmark testSentence.xml newmmodel.txt newsyntmodel.txt output_upper.txt /home/steve/install/TurboParser-2.2.0/TurboParser
 head -n 100 output_upper.txt
-#python toLowerCase.py
 
